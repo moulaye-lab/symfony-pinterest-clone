0b3d7892ebd36f4c1984d6ddd5a19756efc89f42
moulaye-lab <moulayef6@gmail.com>
2020-10-11 02:21:32 +0200

    creation table user avec User:make

--- .history/src/Entity/Pin_20201010195146.php
+++ .history/src/Entity/Pin_20201010195146.php
@@ -146,7 +146,7 @@ class Pin
         if (null !== $imageFile) {
             // It is required that at least one field changes if you are using doctrine
             // otherwise the event listeners won't be called and the file is lost
-            $this->setUpdatedAt(new \DateTimeImmutable);
+            $this->setUpdatedAt = (new \DateTimeImmutable);
         }
     }
 
